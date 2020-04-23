7a03abf1b457a403dfbc68bac0558e9d39402121
lijinze <lijinze@FOUNDER.COM.CN>
2020-04-23 17:53:16 +0800

    end

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>login</div>
+    <router-link to="/home">go to home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
